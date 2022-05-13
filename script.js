aadb4a66a31409b3182b444d170da4b90ed26d92
Daniele <pucciodaniele95@gmail.com>
2022-05-13 15:54:37 +0200

    add dynamic  cells

--- script.js
+++ script.js
@@ -3,3 +3,36 @@
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+const myGrid = document.getElementById("grid");
+
+// Funzione per creare elementi nel DOM
+// Sostituire tagType con il tag che ci serve creare
+const createMyEl = (tagType) => {
+    const myElement = document.createElement(tagType);
+    myElement.className = "square-easy";
+    return myElement;
+}
+
+// ciclo for per creare dinamicamente elementi in pagina
+for (let i = 0; i < 100; i++){
+    const myDivEl = createMyEl ("div");
+    myGrid.append(myDivEl);
+
+    // Cambio colore alla  cella cliccata dall'utente
+    myDivEl.addEventListener("click",
+        function(){
+            this.classList.add("pressed");
+        } 
+    )
+}
+
+
+
+
+
+
+function randomNum(numMin, numMax){
+    let resultNum = Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
+    return resultNum;
+}
